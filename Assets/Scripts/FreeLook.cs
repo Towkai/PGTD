7f6e22d5c00263e8b972e561cef657c65d6e1f2d
Towkai <ji32k7Email@gmail.com>
2026-03-31 10:17:56 +0800

    [Modify]更新InputSystem

--- Assets/Scripts/FreeLook.cs
+++ Assets/Scripts/FreeLook.cs
@@ -1,36 +1,51 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class FreeLook : MonoBehaviour
 {
     // InputAction triggerAction;
+    [SerializeField]
+    PlayerInput playerInput;
     InputAction moveAction; //wsad
     InputAction transAction; //qe
-    public float moveSpeed = 1;
-    public Vector2 rotateSpeed = Vector2.one;
+    public float moveSpeed = 0.1f;
+    public float zoomSpeed = 0.2f;
+    public Vector2 rotateSpeed = Vector2.one * 0.2f;
     [SerializeField]
     Space rotateSpace;
     // InputAction zoomAction;
 
     private void Start()
     {
-        // triggerAction = InputSystem.actions.FindAction("Camera_Trigger");
-        moveAction = InputSystem.actions.FindAction("Camera_Move");
-        transAction = InputSystem.actions.FindAction("Camera_Translate");
+        playerInput ??= this.GetComponent<PlayerInput>();
+        moveAction = playerInput.actions.FindAction("Free_Move");
+        transAction = playerInput.actions.FindAction("Free_Translate");
     }
 
     void Update()
     {
-        Vector2 moveValue = moveAction.ReadValue<Vector2>();
-        float translation = transAction.ReadValue<float>();
-        if (Mouse.current.rightButton.isPressed || Mouse.current.scroll.value != Vector2.zero)
+        if (Mouse.current.rightButton.isPressed)
         {   
-            this.transform.Translate(moveValue.x * moveSpeed, translation * moveSpeed, moveValue.y * moveSpeed);
-            this.transform.Rotate(-Mouse.current.delta.y.value * rotateSpeed.x, Mouse.current.delta.x.value * rotateSpeed.y, 0, rotateSpace);
-            SetRotationZtoZero(this.transform);
+            Move(moveSpeed);
+            Rotate();
+        }
+        if (Mouse.current.scroll.value != Vector2.zero)
+        {
+            Move(zoomSpeed);
         }
     }
-
+    void Move(float speed)
+    {
+        Vector2 moveValue = moveAction.ReadValue<Vector2>();
+        float translation = transAction.ReadValue<float>();
+        this.transform.Translate(moveValue.x * speed, translation * speed, moveValue.y * speed);        
+    }
+    void Rotate()
+    {
+        this.transform.Rotate(-Mouse.current.delta.y.value * rotateSpeed.x, Mouse.current.delta.x.value * rotateSpeed.y, 0, rotateSpace);
+        SetRotationZtoZero(this.transform);
+    }
     void SetRotationZtoZero(Transform transform)
     {
         Vector3 currentEulerAngles = transform.localEulerAngles;
